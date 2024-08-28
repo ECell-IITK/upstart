704d3e34fa59c19c9c49326a0c3782b18be4db28
SARASWAT223 <sarsiit223@gmail.com>
2024-08-28 20:57:33 +0530

    second_commit

--- JS/partner.js
+++ JS/partner.js
@@ -3,7 +3,12 @@ const buttons = document.querySelectorAll('button');
 const sections = document.querySelectorAll('.section');
 
 buttons.forEach((button) => {
+    
     button.addEventListener('click', (e) => {
+        buttons.forEach(button => {
+            button.style.color = 'white';
+        });
+        e.target.style.color = "#00D1A0"
         const sectionId = `section${e.target.id.slice(-1)}`;
         console.log(sectionId);
 
